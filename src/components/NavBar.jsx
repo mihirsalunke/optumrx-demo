250d78a01cb82f61f9ad73176b91191c6974258a
Mihir Salunke <salunke.mi@husky.neu.edu>
2023-08-22 18:51:03 -0700

    removed some items from NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,30 +4,30 @@ const NavBar = () => {
     return (
         <nav class="flex items-center justify-between flex-wrap bg-gray-600 p-6">
             <ul class="flex">
-                <li class="mr-6">
+                {/* <li class="mr-6">
                     <A href="/secure/drug-pricing" class="text-white hover:text-blue-300">Drug Pricing</A>
-                </li>
+                </li> */}
                 <li class="mr-6">
                     <A href="/secure/medication-list" class="text-white hover:text-blue-300">Medication List</A>
                 </li>
-                <li class="mr-6">
+                {/* <li class="mr-6">
                     <A href="/secure/order-builder" class="text-white hover:text-blue-300">Order Builder</A>
-                </li>
-                <li class="mr-6">
+                </li> */}
+                {/* <li class="mr-6">
                     <A href="/secure/order-confirmation" class="text-white hover:text-blue-300">Order Confirmation</A>
-                </li>
+                </li> */}
                 <li class="mr-6">
                     <A href="/secure/order-status" class="text-white hover:text-blue-300">Order Status</A>
                 </li>
-                <li class="mr-6">
+                {/* <li class="mr-6">
                     <A href="/secure/pharmacy-locator" class="text-white hover:text-blue-300">Pharmacy Locator</A>
-                </li>
-                <li class="mr-6">
+                </li> */}
+                {/* <li class="mr-6">
                     <A href="/secure/prescription-drug-list" class="text-white hover:text-blue-300">Prescription Drug List</A>
-                </li>
-                <li class="mr-6">
+                </li> */}
+                {/* <li class="mr-6">
                     <A href="/secure/savings-center" class="text-white hover:text-blue-300">Saving Center</A>
-                </li>
+                </li> */}
                 <li class="mr-6">
                     <A href="/optumrx-demo/custom-widget" class="text-white hover:text-blue-300">Custom Widget Demo</A>
                 </li>
